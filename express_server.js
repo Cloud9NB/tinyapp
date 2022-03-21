9d5dab5d4e512e36812c518d94e2e010a1619fb1
Kevin Lee <blackseeds10@hotmail.com>
2022-03-21 23:52:24 +0000

    added second route and template

--- express_server.js
+++ express_server.js
@@ -1,12 +1,24 @@
+const { text } = require("express");
 const express = require("express");
 const app = express();
 const PORT = 8080;
+
 app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
-}
+};
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
+});
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -23,3 +35,8 @@ app.get("/hello", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+
+// <%for (let key in urls) {%>
+//   <%= key%> - <%=urls[key]%>
+// <%} %>
