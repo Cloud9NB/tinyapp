14b52749062607d684f2630c8364da5da606a6b6
Kevin Lee <blackseeds10@hotmail.com>
2022-03-22 02:25:18 +0000

    added generat random string function

--- express_server.js
+++ express_server.js
@@ -1,3 +1,7 @@
+function generateRandomString() {
+  return Math.random().toString(36).slice(-6)
+}
+
 const express = require("express");
 const app = express();
 const PORT = 8080;
@@ -12,9 +16,10 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
-
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
