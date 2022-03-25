243638855bdde8d85b285d681d6a82ec166d968b
Kevin Lee <blackseeds10@hotmail.com>
2022-03-25 03:20:57 +0000

    added test cases for my getUserByEmail and is passing

---
+++ test/helperTest.js
@@ -0,0 +1,32 @@
+const { assert } = require('chai');
+
+const { getUserByEmail } = require('../helpers.js');
+
+const testUsers = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+};
+
+describe('getUserByEmail', function() {
+  
+  it('should return a user with valid email', function() {
+    const user = getUserByEmail("user@example.com", testUsers);
+    const expectedUserID = "userRandomID";
+    assert.equal(user, expectedUserID);
+  });
+
+  it('should return undefined when they cant find the user with the email address', function() {
+    const user = getUserByEmail("doIexist@test.com", testUsers);
+    const expectedUserID = undefined;
+    assert.equal(user, expectedUserID);
+  });
+
+});
